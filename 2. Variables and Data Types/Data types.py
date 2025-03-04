0287f2fd5a799b6a1f914503aa04dbe2bb79d585
mutai001 <omusekim@gmail.com>
2025-03-04 23:47:09 +0300

    data types

---
+++ 2. Variables and Data Types/Data types.py
@@ -0,0 +1,12 @@
+# Python provides several built-in data types:
+
+
+# Data Type	Example	Description
+# int	x = 10	Integer (whole number)
+# float	y = 3.14	Decimal or floating-point number
+# str	name = "Cyrus"	Text (string)
+# bool	is_active = True	Boolean (True or False)
+# list	fruits = ["apple", "banana"]	Ordered, mutable collection
+# tuple	point = (4, 5)	Ordered, immutable collection
+# dict	person = {"name": "Alice", "age": 25}	Key-value pair storage
+# set	unique_nums = {1, 2, 3}	Unordered, unique elements
