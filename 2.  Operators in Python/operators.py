b00259d130d1451e73ed5bca8aa4ba154713d9f9
mutai001 <omusekim@gmail.com>
2025-03-05 00:18:03 +0300

    arithmethic and assigment opperators

---
+++ 2.  Operators in Python/operators.py
@@ -0,0 +1,36 @@
+# Operators are symbols that perform operations on variables and values.
+
+# Python provides several types of operators:
+
+# Arithmetic Operators - Used to perform mathematical operations
+# Example: +, -, *, /, %, **, //
+
+   # Addition: x + y (Adds two numbers)
+   # Subtraction: x - y (Subtracts two numbers)
+   # Multiplication: x * y (Multiplies two numbers)
+   # Division: x / y (Divides two numbers)
+   # Modulus: x % y  (Returns the remainder of the division)
+   # Exponentiation: x ** y (Returns x raised to the power of y)
+   # Floor Division: x // y  (Returns the integer part of the division)
+
+
+
+
+# Assignment Operators
+  # Used to assign values to variables
+    # Example: =, +=, -=, *=, /=, %=, **=, //=
+    
+      # x = 5 (Assigns the value 5 to x)
+        # x += 3 (Adds 3 to x)
+        # x -= 3 (Subtracts 3 from x)
+        # x *= 3 (Multiplies x by 3)
+        # x /= 3 (Divides x by 3)
+        # x %= 3 (Assigns the remainder of x divided by 3 to x)
+        # x **= 3 (Raises x to the power of 3)
+        # x //= 3 (Assigns the integer part of x divided by 3 to x)
+        
+# Comparison Operators
+# Logical Operators
+# Identity Operators
+# Membership Operators
+# Bitwise Operators
