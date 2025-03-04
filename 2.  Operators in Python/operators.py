7b219320ee863d92f17f0f066daeef04524240ab
mutai001 <omusekim@gmail.com>
2025-03-05 00:24:07 +0300

    identity and membership operators

--- 2.  Operators in Python/operators.py
+++ 2.  Operators in Python/operators.py
@@ -41,6 +41,24 @@
         # x <= y (Returns True if x is less than or equal to y)
         
 # Logical Operators
+    # Used to combine conditional statements
+    # Example: and, or, not
+    
+      # x < 5 and x < 10 (Returns True if x is less than 5 and less than 10)
+        # x < 5 or x < 10 (Returns True if x is less than 5 or less than 10)
+        # not(x < 5 and x < 10) (Returns True if x is not less than 5 and less than 10)
+        
 # Identity Operators
+    # Used to compare objects
+    # Example: is, is not
+    
+      # x is y (Returns True if x is the same object as y)
+        # x is not y (Returns True if x is not the same object as y)
+        
 # Membership Operators
+    # Used to test if a sequence is present in an object
+    # Example: in, not in
+    
+      # x in y (Returns True if x is present in y)
+        # x not in y (Returns True if x is not present in y)
 # Bitwise Operators
