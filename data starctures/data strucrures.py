5ad46313603bd0b0eb818195732c39a0ddb33808
mutai001 <omusekim@gmail.com>
2025-03-05 23:04:26 +0300

    data structures

---
+++ data starctures/data strucrures.py
@@ -0,0 +1,7 @@
+# Data strustures are used to store and organize data in a computer so that it can be used efficiently.
+# Python provides several built-in data structures:
+
+# Data Structure	Example	Description
+# list	        fruits = ["apple", "banana"]	        Ordered, mutable collection
+# tuple	        point = (4, 5)	                    Ordered, immutable collection
+# dict	        person = {"name": "Alice", "age": 25}	Key-value pair storage
