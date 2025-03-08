b618692de3922c7bbcf185acb30bb9cc343a9062
mutai001 <omusekim@gmail.com>
2025-03-08 20:32:01 +0300

    primitive datasypes

--- data starctures/data strucrures.py
+++ data starctures/data strucrures.py
@@ -3,6 +3,30 @@
 
 #Data structures can be classified into two categories:
 # 1. Primitive Data Structures  - These are the basic data structures that directly operate upon the machine instructions.
+    #They are basic building blocks for data manipulation and contain the simplest values.
+    #Examples: Integers, Floats, Strings, Booleans
+    
+# a. Integers
+x = 5
+print(x)
+print(type(x))
+
+# b. Floats
+y = 5.5
+print(y)
+print(type(y))
+
+# c. Strings
+name = "Alice"
+print(name)
+print(type(name))
+
+# d. Booleans
+z = True
+print(z)
+print(type(z))
+
+
 
 
 # Data Structure	Example	Description
