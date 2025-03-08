cca08e2b47d47beeee37133cd62dca1cebe14181
mutai001 <omusekim@gmail.com>
2025-03-08 20:27:29 +0300

    types of datastructures

--- data starctures/data strucrures.py
+++ data starctures/data strucrures.py
@@ -1,5 +1,9 @@
 # Data strustures are used to store and organize data in a computer so that it can be used efficiently.
-# Python provides several built-in data structures:
+# Python provides several built-in data structures for handing and managing data efficiently.
+
+#Data structures can be classified into two categories:
+# 1. Primitive Data Structures  - These are the basic data structures that directly operate upon the machine instructions.
+
 
 # Data Structure	Example	Description
 # list	        fruits = ["apple", "banana"]	        Ordered, mutable collection
