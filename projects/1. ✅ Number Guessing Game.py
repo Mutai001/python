6d98084601dc06d8f2940ea56d673fa1a5ee04e2
Mutai001 <omusekim@gmail.com>
2025-03-20 18:40:12 +0300

    number guesing game

--- projects/1. ✅ Number Guessing Game.py
+++ projects/1. ✅ Number Guessing Game.py
@@ -1,16 +1,17 @@
+import random  
 
-# Number Gueseing Game 
-import random 
+number = random.randint(1, 100)  
+guess = None  
 
-number = random.randint(1,  100)
-guess = None
+while guess != number:  
+    try:  
+        guess = int(input("Guess a number between 1 and 100: "))  
 
-while guess != number:
-    guess = int(input ("Guess a number betweeen I and 100: "))
-  
-    if guess < number:
-            print("Too low! Try again.")
-    elif guess > number:
-            print("Too high! Try again.")
-    else:
-            print("🎉🎉🎉Correct! You have guesed the number.")
+        if guess < number:  
+            print("Too low! Try again.")  
+        elif guess > number:  
+            print("Too high! Try again.")  
+        else:  
+            print("🎉🎉🎉 Correct! You have guessed the number.")  
+    except ValueError:  
+        print("❌ Invalid input! Please enter a number between 1 and 100.")  
